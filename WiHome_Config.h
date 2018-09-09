eea1a57bfc8931f909a82b190f62e486e77613db
gfattinger <gernot.fattinger@ilutions.net>
2018-09-09 10:21:27 -0400

    Back home

--- WiHome_Config.h
+++ WiHome_Config.h
@@ -1,10 +1,13 @@
 // WiFi Settings 
+#define WLAN_SSID       "ExtremeG"
+#define WLAN_PASS       "rideldibixwpa2"
+
 
 // MDSN Settings
 #define MDNS_CLIENT_NAME "wihome1"
 
 // MQTT Server Settings 
-#define MQTT_SERVER      "raspi2.local"
+#define MQTT_SERVER      "cncmill.local"
 #define MQTT_SERVERPORT  1883                   // use 8883 for SSL
 #define MQTT_USERNAME    ""
 #define MQTT_KEY         ""
