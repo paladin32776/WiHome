a62e896f52c1310d6005a57c2cb3b665bad5e21a
gfattinger <gernot.fattinger@ilutions.net>
2019-01-27 22:24:19 -0500

    Complete overhaul of WiHome software: - All communication encapsulated now in WiHomeComm library - Went away from MQTT to UDP messaging - Discovery of hub in the background happening in WiHomeComm library - All messages are now JSON based - First commit of working version - WiHome version is next major version number: V2.0

--- WiHome.ino
+++ WiHome.ino
@@ -1,281 +1,96 @@
-#include <ESP8266WiFi.h>
-#include <ESP8266WebServer.h>
-#include "Adafruit_MQTT.h"
-#include "Adafruit_MQTT_Client.h"
-#include "WiHome_Support.h"
+#include "WiHomeComm.h"
+#include "NoBounceButtons.h"
+#include "SignalLED.h"
 #include "WiHome_Config.h"
-#include "MQTT_topic.h"
 
-const char version[] = "v1.1";
-const char compile_date[] = __DATE__ " " __TIME__ ;
-
-// Setup Wifi and MQTT Clients
-// Create an ESP8266 WiFiClient object to connect to the MQTT server.
-WiFiClient client;
-
-// Setup the MQTT client class by passing in the WiFi client and MQTT server and login details.
-Adafruit_MQTT_Client* mqtt;
-
-// User data class:
-UserData ud;
-
-// Setup button debouncing object:
+WiHomeComm whc;
+SignalLED led(PIN_LED,SLED_BLINK_FAST_1,PIN_LED_ACTIVE_LOW);
+SignalLED relay(PIN_RELAY,SLED_OFF,PIN_RELAY_ACTIVE_LOW);
 NoBounceButtons nbb;
-// and global variables to hold button IDs:
-int button1;
-
-// Setup led and relay:
-SignalLED led1(PIN_LED, SLED_BLINK_FAST_1, PIN_LED_ACTIVE_LOW);
-SignalLED relay1(PIN_RELAY, SLED_OFF, PIN_RELAY_ACTIVE_LOW);
-
-// Create objects for EnoughTimePassed class:
-EnoughTimePassed etp_MQTT_KeepAlive(MQTT_KEEPALIVE);
-
-// Pointers to Topics for MQTT publish and subscribe:
-MQTT_topic* t_stat_relay_feed;
-MQTT_topic* t_cmd_relay_feed;
-MQTT_topic* t_stat_console_feed;
-
-// Pointers for publishing and subscribe MQTT objects:
-Adafruit_MQTT_Publish* stat_relay_feed;
-Adafruit_MQTT_Subscribe* cmd_relay_feed;
-Adafruit_MQTT_Publish* stat_console_feed;
-
-// Global variable to indicate soft AP mode, wlan and mqtt status, and existence of feeds:
-bool is_softAP = false;
-bool wlan_ok = false;
-bool mqtt_ok = false;
-bool mqtt_feeds_exist = false;
-
-void MQTT_create_feeds()
-{
-  if (!mqtt_feeds_exist)
-  {
-    // Setup MQTT topics for feeds:
-    t_stat_relay_feed = new MQTT_topic(ud.mdns_client_name,"/stat/relay");
-    t_cmd_relay_feed = new MQTT_topic(ud.mdns_client_name, "/cmd/relay");
-    t_stat_console_feed = new MQTT_topic(ud.mdns_client_name,"/stat/console");
-    // Setup MQTT client:
-    // mqtt = new Adafruit_MQTT_Client(&client, ud.mqtt_broker, MQTT_SERVERPORT, MQTT_USERNAME, MQTT_KEY);
-    mqtt = new Adafruit_MQTT_Client(&client, ud.mqtt_broker, MQTT_SERVERPORT, MQTT_USERNAME, MQTT_KEY);
-    // Setup MQTT subscriptions and publications
-    stat_relay_feed = new Adafruit_MQTT_Publish(mqtt, t_stat_relay_feed->topic);
-    cmd_relay_feed = new Adafruit_MQTT_Subscribe(mqtt, t_cmd_relay_feed->topic);
-    stat_console_feed = new Adafruit_MQTT_Publish(mqtt, t_stat_console_feed->topic);
-    mqtt->subscribe(cmd_relay_feed);
-    mqtt_feeds_exist = true;
-  }
-}
-
-
-void MQTT_destroy_feeds()
-{
-  if (mqtt_feeds_exist)
-  {
-    mqtt->unsubscribe(cmd_relay_feed);
-    delete stat_relay_feed;
-    delete cmd_relay_feed;
-    delete stat_console_feed;
-    delete mqtt;
-    delete t_stat_relay_feed;
-    delete t_cmd_relay_feed;
-    delete t_stat_console_feed;
-    mqtt_feeds_exist = false;
-  }
-}
-
+char button;
 
 void setup()
 {
   Serial.begin(115200);
-  delay(2000);
-  Serial.printf("WiHome v0.92\n===========\nAuthor:\nGernot\nFattinger\n");
-  // Configure buttons:
-  button1 = nbb.create(PIN_BUTTON);
-  // Load user data (ssid, password, mdsn name, mqtt broker):
-  ud.load();
-  ud.show();
-  // Create MQTT feeds:
-  MQTT_create_feeds();
+  Serial.println();
+  delay(100);
+  button = nbb.create(PIN_BUTTON);
 }
 
 
-void loop_normal()
+void report_relay_status()
 {
-  // Ensure that WiFi Station connection is alive:
-  // (will make the first connection and automatically reconnect when disconnected)
-  wlan_ok = ConnectStation(ud.wlan_ssid, ud.wlan_pass, ud.mdns_client_name);
-
-  if (wlan_ok)
-    MQTT_create_feeds();
-  else
-    MQTT_destroy_feeds();
-  // Ensure the connection to the MQTT server is alive (also checks wlan connection)
-  // (will make the first connection and automatically reconnect when disconnected)
-  if (wlan_ok)
-    mqtt_ok = MQTT_connect(mqtt);
-  else
-    mqtt_ok = false;
-
-  if (!mqtt_ok)
-    MQTT_destroy_feeds();
-
-  if (mqtt_ok)
-  {
-    // Check subscriptions:
-    Adafruit_MQTT_Subscribe *subscription;
-    while ((subscription = mqtt->readSubscription(10))) // Wait for 10ms for a subscription message
-    {
-      if (subscription == cmd_relay_feed)
-      {
-        String command = (char*)cmd_relay_feed->lastread;
-        Serial.print(F("Received: "));
-        Serial.println(command);
-        if (command.compareTo("on")==0)
-        {
-            Serial.println(F("Turning relay on"));
-            relay1.set(SLED_ON);
-        }
-        if (command.compareTo("off")==0)
-        {
-            Serial.println(F("Turning relay off"));
-            relay1.set(SLED_OFF);
-        }
-        if (command.compareTo("toggle")==0)
-        {
-            Serial.println(F("Toggling relay"));
-            relay1.invert();
-            bool result=false;
-            if (relay1.get()==SLED_ON)
-              result = stat_relay_feed->publish("on");
-            else if (relay1.get()==SLED_OFF)
-              result = stat_relay_feed->publish("off");
-            if (result)
-              Serial.println(F("Ok!"));
-            else
-              Serial.println(F("Failed."));
-        }
-        if (command.compareTo("status")==0)
-        {
-            Serial.println(F("Sending status ..."));
-            bool result=false;
-            if (relay1.get()==SLED_ON)
-              result = stat_relay_feed->publish("on");
-            else if (relay1.get()==SLED_OFF)
-              result = stat_relay_feed->publish("off");
-            if (result)
-              Serial.println(F("Ok!"));
-            else
-              Serial.println(F("Failed."));
-        }
-        if (command.compareTo("pulse")==0)
-        {
-            Serial.println(F("Pulsing relay once"));
-            relay1.set(SLED_PULSE);
-        }
-        if (command.compareTo("version")==0)
-        {
-          char buf[60];
-          sprintf(buf,"Version: %s (%s)",version,compile_date);
-          Serial.println(buf);
-          stat_console_feed->publish(buf);
-        }
-        if (command.compareTo("signal")==0)
-        {
-          char buf[20];
-          long rssi = WiFi.RSSI();
-          sprintf(buf,"RSSI = %d dBm",rssi);
-          stat_console_feed->publish(buf);
-        }
-      }
-    }
-  }
-
-  // Logic for LED status display:
-  if (mqtt_ok)
-  {
-    led1.set(relay1.get());
-  }
-  else if (wlan_ok)
-  {
-    led1.set(SLED_BLINK_FAST_3);
-  }
-  else
-  {
-    led1.set(SLED_BLINK_FAST_1);
-  }
-
-  // LED and RELAY checks:
-  led1.check();
-  relay1.check();
-  // Check buttons:
-  nbb.check();
-
-  // Publish if required:
-  if (nbb.action(button1)==1)
-  {
-    Serial.print(F("\nButton1 pressed ..."));
-    relay1.invert();
-    if (mqtt_feeds_exist)
-    {
-      bool result=false;
-      if (relay1.get()==SLED_ON)
-        result = stat_relay_feed->publish("on");
-      else if (relay1.get()==SLED_OFF)
-        result = stat_relay_feed->publish("off");
-      if (result)
-        Serial.println(F("Ok!"));
-      else
-        Serial.println(F("Failed."));
-    }
-    nbb.reset(button1);
-  }
-
-  if (nbb.action(button1)==2)
-  {
-    Serial.println("Going into SoftAP mode ...");
-    is_softAP = true;
-    MQTT_destroy_feeds();
-    led1.set(SLED_BLINK_SLOW); // Blink led slow in SoftAP mode
-    nbb.reset(button1);
-  }
-
-  // ping the server to keep the mqtt connection alive
-  // NOT required if you are publishing once every KEEPALIVE seconds
-  if (etp_MQTT_KeepAlive.enough_time() && WiFi.isConnected() && mqtt_ok)
-    if(! mqtt->ping())
-    {
-      mqtt->disconnect();
-      mqtt_ok=false;
-      MQTT_destroy_feeds();
-    }
-}
-
-
-void loop_softAP()
-{
-  ConnectSoftAP("WiHome_Config_AP", &ud);
-
-  // LED checks:
-  led1.check();
-  // Check buttons
-  nbb.check();
-
-  if (nbb.action(button1)==1)
-  {
-    Serial.println("Going back to Station mode ...");
-    is_softAP = false;
-    MQTT_create_feeds();
-    led1.set(SLED_OFF); // Turn off led when going to station mode
-    nbb.reset(button1);
-  }
+  Serial.printf("Reporting relay status (now %d).\n",relay.get());
+  DynamicJsonBuffer jsonBuffer;
+  JsonObject& root = jsonBuffer.createObject();
+  root["cmd"]="info";
+  root["parameter"]="relay";
+  root["value"]=relay.get();
+  whc.send(root);
 }
 
 
 void loop()
 {
-  if (is_softAP)
-    loop_softAP();
+  DynamicJsonBuffer jsonBuffer;
+
+  // Handling routines for various libraries used:
+  JsonObject& root = whc.check(&jsonBuffer);
+  nbb.check();
+  led.check();
+  relay.check();
+
+  // Logic for LED status display:
+  if (whc.status()==1)
+    led.set(relay.get());
+  else if (whc.status()==2)
+    led.set(SLED_BLINK_FAST_3);
+  else if (whc.status()==3)
+    led.set(SLED_BLINK_FAST_1);
+  else if (whc.status()==4)
+    led.set(SLED_BLINK_SLOW);
   else
-    loop_normal();
+    led.set(SLED_BLINK_FAST);
+
+  // React to button actions:
+  if (nbb.action(button)==2)
+  {
+    Serial.printf("Button1 pressed (action=2).\n");
+    Serial.printf("Attempting to go to SoftAP mode.\n");
+    whc.softAPmode=true;
+    nbb.reset(button);
+  }
+  if (nbb.action(button)==1)
+  {
+    Serial.printf("Button1 pressed (action=1).\n");
+    relay.invert();
+    Serial.printf("LED is now %d.\n",relay.get());
+    report_relay_status();
+    nbb.reset(button);
+  }
+
+  // React to received JSON command objects:
+  if (root!=JsonObject::invalid())
+  {
+    if (root["cmd"]=="set")
+    {
+      if (root["parameter"]=="relay")
+      {
+        relay.set((int)root["value"]);
+        report_relay_status();
+      }
+    }
+    else if (root["cmd"]=="get")
+    {
+      if (root["parameter"]=="relay")
+        report_relay_status();
+      else if (root["parameter"]=="signal")
+      {
+        root["cmd"] = "info";
+        root["value"] = WiFi.RSSI();
+        whc.send(root);
+      }
+    }
+  }
+
 }
--- WiHome_Config.h
+++ WiHome_Config.h
@@ -1,37 +1,6 @@
-#include "Arduino.h"
-
-// WiFi Settings
-// #define WLAN_SSID       ""
-// #define WLAN_PASS       ""
-
-// MDSN Settings
-// #define MDNS_CLIENT_NAME ""
-
-// MQTT Server Settings
-// #define MQTT_SERVER      ""
-#define MQTT_SERVERPORT  1883                   // use 8883 for SSL
-#define MQTT_USERNAME    ""
-#define MQTT_KEY         ""
-#define MQTT_KEEPALIVE   300000
-#define MAX_MQTT_CONNECT_COUNT 10
-
-// Pin configurations SparkFun Dev Thing:
-// #define PIN_LED     5
-// #define PIN_BUTTON  12
-// #define PIN_RELAY   13
-// #define PIN_LED_ACTIVE_LOW true
-// #define PIN_RELAY_ACTIVE_LOW false
-
 // Pin config Gosund wall switch
 #define PIN_LED     13
 #define PIN_BUTTON  0
 #define PIN_RELAY   12
 #define PIN_LED_ACTIVE_LOW true
 #define PIN_RELAY_ACTIVE_LOW false
-
-// Pin configurations ESP-01: *** so far not working ***
-// #define PIN_LED     13
-// #define PIN_BUTTON  0
-// #define PIN_RELAY   13
-// #define PIN_LED_ACTIVE_LOW true
-// #define PIN_RELAY_ACTIVE_LOW false
