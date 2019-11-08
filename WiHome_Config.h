44704b4a6e877de15b509cef8232fabc33236dc0
Gernot Fattinger <gernot.fattinger@ilutions.net>
2019-11-08 16:16:06 +0100

    Update WiHome_Config.h
    Added Sonoff Touch V1.0 config.

--- WiHome_Config.h
+++ WiHome_Config.h
@@ -15,7 +15,14 @@
 // #define PIN_LED_ACTIVE_LOW true
 // #define PIN_RELAY_ACTIVE_LOW false
 
-// Pin config Sonoff Basic RF R2 POWER V1.0
+// // Pin config Sonoff Basic RF R2 POWER V1.0
+// #define PIN_LED     13
+// #define PIN_BUTTON  0
+// #define PIN_RELAY   12
+// #define PIN_LED_ACTIVE_LOW true
+// #define PIN_RELAY_ACTIVE_LOW false
+
+// Pin config Sonoff Touch EU WIFI V1.0 (2016-8-3)
 #define PIN_LED     13
 #define PIN_BUTTON  0
 #define PIN_RELAY   12
